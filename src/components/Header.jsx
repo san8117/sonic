da727ca5f599d4330bfb3e6699aaf4643612cb16
EA - Guilherme Oliveira De Santana <oliveira_santana01@educadventista.org>
2024-05-02 07:26:25 -0300

    Ligações das páginas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import Icone from "../assets/IconedoSonic.jpg"
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
@@ -9,9 +10,27 @@ function Header() {
           <img className="w-14" src={Icone} alt="Ícone do Sonic" /> 
           <h1 className="text-stone-50  text-3xl font-extrabold	">SONIC</h1>
         </div>
-
+        
 
         <div className="flex items-center space-x-4 text-stone-50 text-xl">
+
+        <nav className="">
+          <ul className="flex items-center mr-5">
+            <li>
+              <Link to = "/" className="">Home</Link> 
+            </li>
+
+            <li>
+              <Link to = "/Elenco">Elenco</Link> 
+            </li>
+
+            <li>
+              <Link to = "/Sobre">Sobre</Link> 
+            </li>
+
+           
+          </ul>
+        </nav>
           
         </div>
       </div>
